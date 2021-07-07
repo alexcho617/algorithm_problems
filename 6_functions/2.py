753751523c015593157e2a8096ed58c71f0b821c
alex <alexcho617@icloud.com>
2021-07-07 09:21:41 +0900

    selfNumber

--- 6_functions/2.py
+++ 6_functions/2.py
@@ -1,19 +1,26 @@
-def getSum(n):
-    sum1 = 0
-    while (n != 0):
-        sum1 = sum1 + n % 10 #get last digit
-        n = n // 10 #advance to next digit
-    return sum1
- 
-def isSelfNum(n):
-    for m in range(1, n + 1):
-        if (m + getSum(m) == n):#loop from 1 to n, add the sum of digits till n is found.
-            return False
-    return True
- 
-n = 221
-if (isSelfNum(n)):
-    print("Yes")
- 
-else:
-    print("No")
+def self_number():
+    li = []
+    for i in range(1,10000):
+        li.append(i + sum([int(j) for j in str(i)]))
+    return li
+
+li = self_number()
+print(li)
+
+#Way too slow. Time outs
+# def digitSum(n): #return the sum of all the digits
+#     sum = 0
+#     while (n != 0):
+#         sum = sum + n % 10
+#         n = n // 10
+#     return sum
+
+# def isSelf(n):#iterate from 1 to n and add the sums. escape on match
+#     for i in range(1,n+1):
+#         if(i + digitSum(i) == n):
+#             return False #meaning it can be generated
+#     return True #meaning it cannot be generated
+
+#for i in range(1,10000):
+    # if isSelf(i) == True:
+    #     print(i)

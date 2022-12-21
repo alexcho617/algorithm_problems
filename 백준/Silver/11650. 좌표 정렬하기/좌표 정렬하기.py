88b3c11bc4207005f0a64a3ed9f9267f0e283f38
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2022-12-21 14:23:03 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 356 ms, Memory: 57592 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,8 +1,15 @@
-arr = []
-for _ in range(int(input())):
-    arr.append(list(map(int, input().split())))
- 
-arr.sort(key=lambda x:(x[0], x[1]))
- 
-for e in arr:
-    print(str(e[0]) + " " + str(e[1]))
+# https://www.acmicpc.net/problem/11650
+# 좌표 정렬하기
+import sys
+n = int(sys.stdin.readline())
+inputArr = []
+for i in range(n):
+    inputArr.append(list(map(int,sys.stdin.readline().split()))) #int로 해야 비교가 가능했음
+
+inputArr.sort(key=lambda x: (x[0], x[1])) #
+# sortedArr = sorted(inputArr)
+# for i in sortedArr:
+#     print(*i)
+for a in inputArr:
+    print(str(a[0]) + ' '+str(a[1]))
+

38be00ab1a18a23c2538b70d9dcc4f2ee34f463f
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-01-03 17:24:05 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/1991
+
+import sys 
+n = int(sys.stdin.readline().strip())
+tree = {}
+
+for _ in range(n):
+    root ,left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left,right]
+
+def preorder(root):
+    if root != '.':
+        print(root, end = '')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end= '')
+        inorder(tree[root][1])
+
+def postOrder(root):
+    if root != '.':
+        postOrder(tree[root][0])
+        postOrder(tree[root][1])
+        print(root, end = '')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postOrder('A')

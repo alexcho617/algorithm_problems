76af40bc5bd426a00cd86af237a3af87bf07c407
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-01-04 18:21:15 +0900

    [Silver II] Title: 종이의 개수, Time: 4752 ms, Memory: 112608 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -27,43 +27,32 @@ def check_all_plus_one(page: list):
     return True
 
 
-def divide(graph: list):
+def divide(graph: list, n):
     s1, s2, s3, s4, s5, s6, s7, s8, s9 = [], [], [], [], [], [], [], [], []
 
-    onethird = len(graph) // 3
-    twothird = len(graph) // 3 * 2
+    onethird = n // 3
 
     for i in range(onethird):
         s1.append(graph[i][:onethird])
-        s2.append(graph[i][onethird:twothird])
-        s3.append(graph[i][twothird:])
+        s2.append(graph[i][onethird:onethird*2])
+        s3.append(graph[i][onethird*2:])
 
-    for i in range(onethird, twothird):
+    for i in range(onethird, onethird*2):
         s4.append(graph[i][:onethird])
-        s5.append(graph[i][onethird:twothird])
-        s6.append(graph[i][twothird:])
+        s5.append(graph[i][onethird:onethird*2])
+        s6.append(graph[i][onethird*2:])
 
-    for i in range(twothird, len(graph)):
+    for i in range(onethird*2, onethird*3):
         s7.append(graph[i][:onethird])
-        s8.append(graph[i][onethird:twothird])
-        s9.append(graph[i][twothird:])
+        s8.append(graph[i][onethird:onethird*2])
+        s9.append(graph[i][onethird*2:])
 
     return s1, s2, s3, s4, s5, s6, s7, s8, s9
 
 
 def recursion(graph: list, n: int):
     global negs, zeros, pos
-    # if all(0 not in l for l in graph):
-    #     if all(-1 not in l for l in graph):
-    #         pos += 1
-    #         return
-    #     elif all(1 not in l for l in graph):
-    #         negs += 1
-    #         return
-    # if all(1 not in l for l in graph):
-    #     if all(-1 not in l for l in graph):
-    #         zeros += 1
-    #         return
+
     if check_all_minus_one(graph):
         negs += 1
         return
@@ -73,9 +62,8 @@ def recursion(graph: list, n: int):
     if check_all_plus_one(graph):
         pos += 1
         return
-
     else:
-        for s in divide(graph):
+        for s in divide(graph,n):
             recursion(s, n//3)
 
 

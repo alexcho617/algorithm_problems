85403af732c0d36c95499865cf5fa1f014ee5b55
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-01-05 15:18:17 +0900

    [Silver III] Title: Meeting Time, Time: 80 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/10678. Meeting Time/Meeting Time.py
@@ -0,0 +1,46 @@
+# https://www.acmicpc.net/problem/10678
+from collections import deque
+
+# get graphs for elsie and bessie
+
+edge1 = {}
+edge2 = {}
+n, m = map(int, input().split())
+for i in range(1, n):
+    edge1[i] = []
+    edge2[i] = []
+
+for _ in range(m):
+    a, b, c, d = map(int, input().split())
+    edge1[a].append([b, c])
+    edge2[a].append([b, d])
+
+# perform dfs
+
+
+def dfs(graph, dist, cur, sums):
+    global n
+    # arrive at n, save accumulated sums
+    if cur == n:
+        dist.append(sums)
+        return
+    # otherwise continue search
+    for adjNodes in graph[cur]:
+        dfs(graph, dist, adjNodes[0], sums + adjNodes[1])
+    return dist
+   
+# compare and get results
+dist1 = []
+dist2 = []
+
+dist1 = dfs(edge1, dist1, 1, 0)
+dist2 = dfs(edge2, dist2, 1, 0)
+dist1.sort()
+for d in dist1:
+    if d in dist2:
+        print(d)
+        quit()
+print("IMPOSSIBLE")
+
+    
+

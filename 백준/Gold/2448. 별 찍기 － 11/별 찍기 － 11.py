ba795991a7cf84b7b659d153023b6d41b62addd9
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-01-06 13:33:01 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 100 ms, Memory: 72284 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/2448
+
+def appendStars(n):
+    # base case
+    if n == 3:
+        return ["  *  ", " * * ", "*****"]
+    # print
+    stars = appendStars(n//2)
+    temp = []
+    for s in stars:
+        temp.append(" "*(n//2)+s+' '*(n//2))
+    for s in stars:
+        temp.append(s+" "+s)
+    return temp
+    # recursive
+
+
+n = int(input())
+print("\n".join(appendStars(n)))

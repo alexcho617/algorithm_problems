0a49603edadc914b313b535c4f84bf71e3da7df1
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-01-09 16:55:58 +0900

    [Silver IV] Title: 30, Time: 108 ms, Memory: 31592 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/10610
+
+num = list(input())
+sum = 0
+for n in num:
+    
+    sum += int(n)
+if (sum % 3) == 0: # multiple of 3
+    if '0' in num: #multiple of 10
+        num.sort(reverse = True) #reverse order
+        for n in num: print(n, end = '')
+        print()
+    else:
+        print('-1')
+        quit()
+else: 
+    print('-1')
+    quit()

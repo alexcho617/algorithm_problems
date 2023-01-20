4055f60b728c15bfa70ac4f1e7c7ce8dbbe4b439
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-01-20 16:19:04 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -2,16 +2,22 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-counsels =[]
+counsels = []
 for _ in range(n):
     counsels.append(list(map(int,input().split())))
-counsels.append([0,0])
-
 dp = [0 for _ in range(n+1)]
 
+#init first iteration
+counsels.append([0,0]) # for last index
+
+#start from behind
 for i in range(n-1,-1,-1):
-    if counsels[i][0] + i <= n:
+    if counsels[i][0] + i <= n: #include if they are same
+        #perform optimization
+        #since dp is passed down, compare dp[i+1](previous)dp with counsel[i][1] + dp[i+ offset]
         dp[i] = max(dp[i+1], dp[i + counsels[i][0]] + counsels[i][1])
     else:
+        #take previous item
         dp[i] = dp[i+1]
 print(max(dp))
+

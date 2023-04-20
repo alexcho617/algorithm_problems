9f55f80d40d0c95fbe51c1b79f4612cf666dea9d
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-04-20 12:57:56 +0900

    [level 1] Title: K번째수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,11 +1,8 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        # print(command)
         i,j,k = command
         temp = array[i-1:j]
-        # print("Splice:",temp)
         temp.sort()
-        # print("kth item",temp[k-1])
         answer.append(temp[k-1])
     return answer

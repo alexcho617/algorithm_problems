67ec2080d62a0b175f0035d083259ab3055b6e20
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-06-02 12:34:24 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,32 @@
+def solution(genres, plays):
+    answer = []
+    
+    #hash1 genre:totalPlays
+    hash1 = {}
+    #hash2 genre: [id,play]
+    hash2 = {}
+    #hash3 genre: [one or two songs]
+    for i in range(len(genres)):
+        genre,play = genres[i], plays[i]
+        
+        if genre in hash1:
+            hash1[genre] += plays[i]
+        else:
+            hash1[genre] = plays[i]
+        
+        if  genre in hash2:
+            hash2[genre].append([play,i])
+        else:
+            hash2[genre] = [[play,i]]
+            
+    hash1 = dict(sorted(hash1.items(), key = lambda x: x[1], reverse = True))
+    for key in hash1:
+        temp = sorted(hash2[key],key = lambda x: (x[0],-int(x[1])), reverse = True)
+        if len(temp) > 1:
+            answer.append(temp[0][1])
+            answer.append(temp[1][1])
+        else:
+            answer.append(temp[0][1])
+
+    
+    return answer

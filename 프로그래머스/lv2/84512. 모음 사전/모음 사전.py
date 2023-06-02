99fcc2b681a32818cec7706bdd598a27c6c51cbe
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-06-02 11:19:42 +0900

    [level 2] Title: 모음 사전, Time: 1.32 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -6,13 +6,13 @@ def solution(word):
     for i in range(1,6):
         wordPool += list(itertools.product(alphabet,repeat = i))
     for wordItem in wordPool:
-        temp = ""
-        for character in wordItem:
-            temp += character
-        wordDict.append(temp)
+        # temp = ""
+        # for character in wordItem:
+        #     temp += character
+        wordDict.append("".join(wordItem))
     
     wordDict.sort()
-    temp = ""
-    for w in word:
-        temp += w
+    temp = "".join(word)
+    # for w in word:
+    #     temp += w
     return wordDict.index(temp) + 1

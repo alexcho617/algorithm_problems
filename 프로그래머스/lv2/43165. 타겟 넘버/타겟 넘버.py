1b1597a86e0ca2d7d448c0d9b8ae48f896544a1b
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-06-04 16:16:30 +0900

    [level 2] Title: 타겟 넘버, Time: 2.92 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,27 +1,55 @@
-#uses product to find all combinations and sums it to find target
-# from itertools import product
+#uses recursive dfs to reach target but use memoization to imporve time complexity
+def solution(numbers,target):
+    answer = 0
+    def dfs(numbers, target, curr, index):
+        
+        #escape end of tree
+        if index == len(numbers):
+            if curr == target:
+                return 1
+            else:
+                return 0
+        
+        #recursive call
+        count = dfs(numbers, target, curr + numbers[index], index + 1) + dfs(numbers, target, curr - numbers[index], index + 1)
+        return count
+    
+    answer = dfs(numbers,target,0,0)
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def solution(numbers, target):
-#     l = [(x, -x) for x in numbers]
-#     print(l)
-#     s = list(map(sum, product(*l)))
-#     return s.count(target)
+#     memo = {}
 
-#uses recursive dfs to reach target but use memoization to imporve time complexity
-def solution(numbers, target):
-    memo = {}
+#     def dfs(nums, target, curr, idx):
+#         if idx == len(nums):
+#             return 1 if curr == target else 0
 
-    def dfs(nums, target, curr, idx):
-        if idx == len(nums):
-            return 1 if curr == target else 0
+#         if (idx, curr) in memo:
+#             return memo[(idx, curr)]
 
-        if (idx, curr) in memo:
-            return memo[(idx, curr)]
+#         count = dfs(nums, target, curr + nums[idx], idx + 1) + dfs(nums, target, curr - nums[idx], idx + 1)
+#         memo[(idx, curr)] = count
 
-        count = dfs(nums, target, curr + nums[idx], idx + 1) + dfs(nums, target, curr - nums[idx], idx + 1)
-        memo[(idx, curr)] = count
+#         return count
 
-        return count
-
-    answer = dfs(numbers, target, 0, 0)
-    return answer
+#     answer = dfs(numbers, target, 0, 0)
+#     return answer

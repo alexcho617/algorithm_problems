65bb4c8f310fc62d9468c538dbe458055d524c78
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-06-11 19:20:30 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,8 +1,12 @@
 def solution(citations):
     answer = 0
+    #내림차순 정렬을 하여 index 와 citation 값을 비교한다
     citations.sort(reverse = True)
+    
     for idx, citation in enumerate(citations):
-        print(idx, citation)
+            #현재 인덱스가 인용된 수보다 같거나 큰 순간에 최대 H-Index가 된다.
+            #내림 차순 정렬이기 때문에 그 밑으로의 논문들은 이미 인용된 수보다 낮다.
         if citation <= idx:
             return idx
+    #만족하는 수가 없는 경우에 최대치 리턴
     return len(citations)

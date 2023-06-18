2fa5ad5a4d02c762551e065ee33c8037bc9ad3e2
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-06-18 21:41:31 +0900

    [level 1] Title: 폰켓몬, Time: 1.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,11 @@
+from collections import defaultdict
 def solution(nums):
     #make hash
-    hash = {}
+    hash = defaultdict(int)
     #put array into hash and count
     for num in nums:
-        if num in hash:
-            hash[num] += 1
-        else:
-            hash[num] = 1
+        hash[num] += 1
+        
     #answer is length of hash keys
     answer = min(len(hash.keys()),len(nums)/2)
     return answer

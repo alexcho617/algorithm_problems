95d9dc76705194de2d29ab5cfbe25127b4fe4900
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-06-18 19:50:00 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,11 +1,13 @@
+from collections import defaultdict
 def solution(clothes):
-    hash = {}
+    hash = defaultdict(int)
     #hash
     for name, kind in clothes:
-        if kind in hash:
-            hash[kind] += 1
-        else:
-            hash[kind] = 1
+        hash[kind] += 1
+        # if kind in hash:
+        #     hash[kind] += 1
+        # else:
+        #     hash[kind] = 1
     print(hash)
     #number of items + hash value multplication
     values = hash.values()
